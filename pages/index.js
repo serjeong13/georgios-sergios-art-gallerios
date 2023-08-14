5d5d7b59db363f6b6b6689a3500a57161dcc4ed1
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-08-14 16:06:07 +0200

    added structure into index.js, no components yet

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function HomePage() {
+import useSWR from "swr";
+import Image from "next/image";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function App() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
       <h1>Georgios-Sergios-Horses</h1>
+      <ul>
+        {data.map((artPiece) => (
+          <li key={artPiece.id}>
+            <h2>{artPiece.title}</h2>
+            <Image
+              src={artPiece.image}
+              alt={artPiece.title}
+              width={200}
+              height={200}
+            />
+            <p>{artPiece.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
